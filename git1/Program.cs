e6526444930dca75d87e86edbeb3844625c9b01a
Vladik Bulkin <vladikkrivobrodskiy@tut.by>
2023-05-23 22:12:09 +0300

    first commit

---
+++ git1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите размер массива: ");
+            int size;
+            size = int.Parse(Console.ReadLine());
+            int[] mas = new int[size];
+            Random rand = new Random();
+            for (int i = 0; i < size; i++)
+            {
+                mas[i] = rand.Next(-9, 10);
+            }
+            Console.Write("Массив: |");
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write(mas[i] + " ");
+            }
+            Console.Write("|\n");
+            int sum = 0;
+            for (int i = 0; i < size; i++)
+            {
+                if (mas[i] < 0)
+                {
+                    sum += mas[i];
+                }
+            }
+            Console.WriteLine("Суdwe: " + sum);
+        }
+    }
+}
